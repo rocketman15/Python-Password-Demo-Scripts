206d5484e212053135c6a67b1d8099ceb0660348
rocketman15 <32797472+rocketman15@users.noreply.github.com>
2023-03-15 22:16:15 -0500

    Update stringHasher.py

--- stringHasher.py
+++ stringHasher.py
@@ -1,6 +1,6 @@
-import hashlib
-input = input("enter a string to be hashed ")
+import hashlib# import the hashlib library 
+input = input("enter a string to be hashed ") # get user input
 def hashString(string):
     result = hashlib.md5(string.encode())
     return(result.hexdigest())
-print(hashString(input))
+print(hashString(input))#hash the string
