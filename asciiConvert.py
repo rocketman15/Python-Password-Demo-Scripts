0d244fefb25a3a8256ffed5a01314f3c69ffb7ab
rocketman15 <32797472+rocketman15@users.noreply.github.com>
2023-03-15 22:19:54 -0500

    Update asciiConvert.py

--- asciiConvert.py
+++ asciiConvert.py
@@ -1,9 +1,9 @@
-input = input("enter a string to convert to ascii")
+input = input("enter a string to convert to ascii")# get user input 
 
-def encrypt(message):
+def encrypt(message):# this function will encrypt the input 
     encryptedString = ""
     for i in message:
         encryptedString += str(ord(i))
         encryptedString += " "
     return(encryptedString)
-print(encrypt(input))
+print(encrypt(input))#print the encrypted input 
