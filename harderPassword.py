cae419f201bc55c5401395f4131c9a50c3ab03f1
rocketman15 <32797472+rocketman15@users.noreply.github.com>
2023-03-15 22:19:04 -0500

    Update harderPassword.py

--- harderPassword.py
+++ harderPassword.py
@@ -1,7 +1,8 @@
-encrypted_password = "83 117 98 115 99 114 105 98 101 "
-userInput = input("enter a password to unlock the program ")
+encrypted_password = "83 117 98 115 99 114 105 98 101 "# this is the ascii value for the password. make sure to put a single space between values, 
+#and that there are no extra spaces, except a single space at the end
+userInput = input("enter a password to unlock the program ")#get user input
 
-def encryptPassword(message):
+def encryptPassword(message):#this function will encrypt the password
     encryptedString = ""
     for i in message:
         encryptedString += str(ord(i))
@@ -15,11 +16,11 @@ def comparePasswords(encryptedUserPassword, correctEncryptedPassword):
     else:
         return(0)
 
-def main():
+def main():# put the code that you want to run after a successful unlock here. 
     print("Access Granted. Are you a subscriber?")
 
 encryptedInput = encryptPassword(userInput)
 if comparePasswords(encryptedInput, encrypted_password) == 1:
     main()
-else:
+else:# put the code that you want to run after a failed unlock here. 
     print("Nice Try. Subscribe, then try again")
