30f5de19f75f60f9d2ef39d84855754b4e16d47f
rocketman15 <32797472+rocketman15@users.noreply.github.com>
2023-03-15 22:21:33 -0500

    Update easyPassword.py

--- easyPassword.py
+++ easyPassword.py
@@ -1,13 +1,13 @@
-user_password = "Like"
-user_input = input("enter the correct password to access the program ")
-def compare_passwords(correct_password, user_input):
+user_password = "Like"# set a string for the password 
+user_input = input("enter the correct password to access the program ")# get user input
+def compare_passwords(correct_password, user_input):# compare the passwords 
     if user_input == correct_password:
         return(1)
     else:
         return(0)
-def main():
+def main():# put the code that you want to run after a successful unlock here 
     print("unlock successful")
 if compare_passwords(user_password, user_input) == 1:
     main()
-else:
+else:# put the code you want to run after a failed unlock here. 
     print("sorry. incorect password. Like the video, then try again")
